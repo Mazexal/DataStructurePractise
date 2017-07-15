a354f85bd5f8f2e275f34b323956b5a55defd8a4
Mazexal <1023520044@qq.com>
2017-07-15 22:08:09 +0800

    Small practise

--- src/main/java/ExecuteProgram.java
+++ src/main/java/ExecuteProgram.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 /**
  * Created by 10235 on 2017/7/14.
  */
@@ -5,6 +7,9 @@ public class ExecuteProgram {
     public  static  void main(String[] args)
     {
         System.out.println("Begin");
+        Random random=new Random(47);
+        
+
     }
 
 
