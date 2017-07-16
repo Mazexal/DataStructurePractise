886a36cee153780cfc6272c205a268c880b6e9fb
Mazexal <1023520044@qq.com>
2017-07-16 21:11:48 +0800

    LinkPractise

---
+++ src/main/java/CP1/TreeNode.java
@@ -0,0 +1,15 @@
+package CP1;
+
+/**
+ * Created by 10235 on 2017/7/16.
+ */
+public class TreeNode {
+    public int val;
+    public TreeNode left,right;
+    public TreeNode(int val)
+    {
+        this.val=val;
+        this.left=null;
+        this.right=null;
+    }
+}
