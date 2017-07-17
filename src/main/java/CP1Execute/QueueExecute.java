d7be4bf151c86b7d02316450966c79a2c65abe8f
Mazexal <1023520044@qq.com>
2017-07-17 19:42:49 +0800

    LinkPractise

--- src/main/java/CP1Execute/QueueExecute.java
+++ src/main/java/CP1Execute/QueueExecute.java
@@ -1,5 +1,7 @@
 package CP1Execute;
 
+import com.sun.org.apache.xpath.internal.operations.String;
+
 import java.util.*;
 import java.util.concurrent.PriorityBlockingQueue;
 
@@ -11,14 +13,30 @@ public class QueueExecute {
 
 
         //队列
-        Queue<Integer> queue=new LinkedList<Integer>();
-        int qLneg=queue.size();
-        PriorityQueue<Integer> priorityQueue=new PriorityQueue<Integer>();//非线程安全
+        Queue<Integer> queue = new LinkedList<Integer>();
+        int qLneg = queue.size();
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<Integer>();//非线程安全
 
-        PriorityBlockingQueue<Integer> priorityBlockingQueue=new PriorityBlockingQueue<Integer>();//线程安全
+        PriorityBlockingQueue<Integer> priorityBlockingQueue = new PriorityBlockingQueue<Integer>();//线程安全
 
-        Deque<Integer> deque=new ArrayDeque<Integer>();//1.6后特性 双端队列
+
+        Deque<Integer> deque = new ArrayDeque<Integer>();//1.6后特性 双端队列
         deque.addFirst(10);
         deque.removeFirst();
+        deque.pop();
+        deque.push(2);
+
+        Set<String> hash = new HashSet<String>();
+      //  hash.add("bill");
+        Boolean b = hash.contains("bill");
+
+
+        Map<String,Integer> map=new HashMap<>();
+//        map.put("bill",98);
+//        map.put("tom",12);
+        boolean exits=map.containsKey("tom");
+        int point=map.get("tom");
+        point=map.remove("tom");
+        //Set<String> set=map.keySet();
     }
 }
