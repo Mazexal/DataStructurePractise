606cb7d45f895036d54af08e5f2d8defee7f7be7
Mazexal <1023520044@qq.com>
2017-07-18 11:26:05 +0800

    SortCp

---
+++ src/main/Readme.txt
@@ -0,0 +1,4 @@
+书籍是algorithm-ebook
+
+CP1是基础数据结构
+CP2是常用排序算法
---
+++ src/main/java/CP2/SortUtil.java
@@ -0,0 +1,14 @@
+package CP2;
+
+/**
+ * Created by Mazexal on 2017/7/18.
+ */
+public class SortUtil {
+
+    public static void bubbleSort(int[] array)
+    {
+
+
+    }
+
+}
