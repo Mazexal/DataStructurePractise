a3fa63d0c34fa98664fbec7d93aa05908e00322f
Mazexal <1023520044@qq.com>
2017-07-20 21:31:59 +0800

    CP2 sort

--- src/main/java/CP2/SortUtil.java
+++ src/main/java/CP2/SortUtil.java
@@ -25,28 +25,41 @@ public class SortUtil {
     }
 
     //选择排序
-    public static void selectionSort(int[] array)
-    {
-        int len=array.length;
-        for(int i=0;i<len;i++)
-        {
+    public static void selectionSort(int[] array) {
+        int len = array.length;
+        for (int i = 0; i < len; i++) {
             for (int item : array) {
                 System.out.print(item + " ");
             }
             System.out.println();
-            int min_index=i;
-            for(int j=i+1;j<len;j++){
-                if(array[j]<array[min_index]){
-                    min_index=j;
+            int min_index = i;
+            for (int j = i + 1; j < len; j++) {
+                if (array[j] < array[min_index]) {
+                    min_index = j;
                 }
             }
-            int temp=array[min_index];
-            array[min_index]=array[i];
-            array[i]=temp;
+            int temp = array[min_index];
+            array[min_index] = array[i];
+            array[i] = temp;
         }
 
     }
 
+    public static void inserionSort(int[] array) {
+        int len = array.length;
+        for (int i = 0; i < len; i++) {
+            int index = i, array_i = array[i];
+            while (index > 0 && array[index - 1] > array_i) {
+                array[index] = array[index - 1];
+                index -= 1;
+            }
+            array[index] = array_i;
+            for (int item : array) {
+                System.out.print(item + " ");
+            }
+            System.out.println();
+        }
+    }
 
 
 }
