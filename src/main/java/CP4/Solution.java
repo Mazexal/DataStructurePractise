499306df801e44723213217a5eedca37d72f3027
Mazexal <1023520044@qq.com>
2017-07-23 21:43:40 +0800

    CP3sort

---
+++ src/main/java/CP4/Solution.java
@@ -0,0 +1,20 @@
+package CP4;
+
+/**
+ * Created by 10235 on 2017/7/23.
+ */
+public class Solution {
+    public int strStr(String haystack, String needle) {
+        if (haystack == null && needle == null) return 0;
+        if (haystack == null) return -1;
+        if (needle == null) return 0;
+        for (int i = 0; i < haystack.length() - needle.length() + 1; i++) {
+            int j = 0;
+            for (; j < needle.length(); j++) {
+                if (haystack.charAt(i + j) != needle.charAt(j)) break;
+            }
+            if (j == needle.length()) return i;
+        }
+        return -1;
+    }
+}
